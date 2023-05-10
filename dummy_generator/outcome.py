844ee4c1d31979e71cbc5487e4a6f5f0bb0b7fe9
HARRIS KURNIADI SUMBOGO <harris.kurniadi@ti.ukdw.ac.id>
2023-05-11 01:15:44 +0700

    dummy outcome

---
+++ dummy_generator/outcome.py
@@ -0,0 +1,37 @@
+import petl
+import psycopg2
+import os
+import random
+from functools import partial
+
+from dotenv import load_dotenv
+load_dotenv()
+
+dbname = os.getenv("DB_NAME")
+dbuser = os.getenv("DB_USER")
+dbpass = os.getenv("DB_PASS")
+
+connection = psycopg2.connect(
+    f'dbname={dbname} user={dbuser} password={dbpass}')
+
+goal = petl.fromdb(connection, "SELECT * FROM goal")
+
+if petl.nrows(goal) == 0:
+    print("Masukan data goal terlebih dahulu")
+else:
+    n = 60_000
+
+    id_goal = list(goal["id_goal"])
+
+    fields = [
+        ("id_goal", partial(random.choice, id_goal))
+    ]
+
+    dummy_outcome = petl.dummytable(n, fields=fields, seed=42)
+    dummy_outcome = petl.addcolumn(dummy_outcome,
+                                   field="nama_outcome",
+                                   col=[f"outcome{i}" for i in range(n)])
+
+    cursor = connection.cursor()
+    cursor.execute("TRUNCATE outcome RESTART IDENTITY CASCADE")
+    petl.todb(dummy_outcome, cursor, "outcome")
