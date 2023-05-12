886f42a7718186fd37fa042efdf07a7b7882778f
HARRIS KURNIADI SUMBOGO <harris.kurniadi@ti.ukdw.ac.id>
2023-05-12 20:13:47 +0700

    mengubah struktur dim_pekerja dan etl br_pekerja_proyek

---
+++ etl/br_pekerja_proyek.py
@@ -0,0 +1,27 @@
+import petl
+import psycopg2
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+data_mart = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME_DM")} user={os.getenv("DB_USER_DM")} password={os.getenv("DB_PASS_DM")}')
+
+operasional = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASS")}')
+
+pekerja_proyek = petl.fromdb(operasional, "SELECT * FROM pekerja_proyek")
+
+dim_pekerja = petl.fromdb(data_mart, "SELECT * FROM dim_pekerja")
+fact_proyek = petl.fromdb(data_mart, "SELECT * FROM fact_proyek")
+
+lkp_pekerja = petl.dictlookupone(dim_pekerja, "id_pekerja")
+lkp_proyek = petl.dictlookupone(dim_pekerja, "id_proyek")
+
+br_pekerja_proyek = petl.convert(pekerja_proyek, {"id_pekerja": lambda id_pekerja : lkp_pekerja[id_pekerja]["pekerja_key"],
+                                                  "id_proyek": lambda id_proyek : lkp_proyek[id_proyek]["proyek_key"]})
+
+cursor = data_mart.cursor()
+cursor.execute("TRUNCATE br_pekerja_proyek RESTART IDENTITY CASCADE")
+petl.todb(br_pekerja_proyek, cursor, "br_pekerja_proyek")
--- etl/dim_pekerja.py
+++ etl/dim_pekerja.py
@@ -11,13 +11,13 @@ data_mart = psycopg2.connect(
 operasional = psycopg2.connect(
     f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASS")}')
 
-pekerja = petl.fromdb(operasional, "SELECT * FROM pekerja_proyek")
+pekerja = petl.fromdb(operasional, "SELECT * FROM pekerja")
 jabatan = petl.fromdb(operasional, "SELECT * FROM jabatan_proyek")
 lkp_jabatan = petl.dictlookupone(jabatan, "id_jabatan")
 
 pekerja = petl.convert(pekerja, {"id_jabatan": lambda id_jabatan: lkp_jabatan[id_jabatan]["nama_jabatan"]})
 pekerja = petl.rename(pekerja, {"id_jabatan": "jabatan"})
-print(pekerja)
-# cursor = data_mart.cursor()
-# cursor.execute("TRUNCATE dim_pekerja RESTART IDENTITY CASCADE")
-# petl.todb(pekerja, cursor, "dim_pekerja")
+
+cursor = data_mart.cursor()
+cursor.execute("TRUNCATE dim_pekerja RESTART IDENTITY CASCADE")
+petl.todb(pekerja, cursor, "dim_pekerja")
