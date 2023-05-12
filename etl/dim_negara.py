46bccaf2cd0b7ad45d8ddee87ed9c7ec150226f2
HARRIS KURNIADI SUMBOGO <harris.kurniadi@ti.ukdw.ac.id>
2023-05-12 13:55:43 +0700

    etl dim negara

---
+++ etl/dim_negara.py
@@ -0,0 +1,20 @@
+import petl
+import psycopg2
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+data_mart = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME_DM")} user={os.getenv("DB_USER_DM")} password={os.getenv("DB_PASS_DM")}')
+
+operasional = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASS")}')
+
+negara = petl.fromdb(operasional, "SELECT * FROM negara")
+
+negara = petl.cutout(negara, "id_negara")
+
+cursor = data_mart.cursor()
+cursor.execute("TRUNCATE dim_negara RESTART IDENTITY CASCADE")
+petl.todb(negara, cursor, "dim_negara")
