8e1c6ffa233fe38c22e4abc4f5e98bb7175a36b0
HARRIS KURNIADI SUMBOGO <harris.kurniadi@ti.ukdw.ac.id>
2023-05-12 18:57:49 +0700

    update struktur pekerja proyek

--- dummy_generator/pekerja_proyek.py
+++ dummy_generator/pekerja_proyek.py
@@ -15,27 +15,22 @@ connection = psycopg2.connect(
     f'dbname={dbname} user={dbuser} password={dbpass}')
 
 proyek = petl.fromdb(connection, "SELECT * FROM proyek")
-jabatan_proyek = petl.fromdb(connection, "SELECT * FROM jabatan_proyek")
+pekerja = petl.fromdb(connection, "SELECT * FROM pekerja")
 
-if petl.nrows(proyek) == 0 or petl.nrows(jabatan_proyek) == 0:
-    print("Masukan proyek dan jabatan_proyek terlebih dahulu")
+if petl.nrows(proyek) == 0 or petl.nrows(pekerja) == 0:
+    print("Masukan proyek dan pekerja terlebih dahulu")
 else:
     n = 5_000
 
     id_proyek = list(proyek["id_proyek"])
-    id_jabatan = list(jabatan_proyek["id_jabatan"])
-    id_karyawan = [None, ] + [i for i in range(100)]
+    id_pekerja = list(pekerja["id_pekerja"])
 
     fields = [
         ("id_proyek", partial(random.choice, id_proyek)),
-        ("id_jabatan", partial(random.choice, id_jabatan)),
-        ("id_karyawan", partial(random.choice, id_karyawan))
+        ("id_pekerja", partial(random.choice, id_pekerja)),
     ]
 
     dummy_pekerja = petl.dummytable(n, fields=fields, seed=42) 
-    dummy_pekerja = petl.addcolumn(dummy_pekerja,
-                                   field="nama_pekerja",
-                                   col=[f"pekerja{i}" for i in range(n)])
     
     cursor = connection.cursor()
     cursor.execute("TRUNCATE pekerja_proyek RESTART IDENTITY CASCADE")
---
+++ etl/dim_pekerja.py
@@ -0,0 +1,23 @@
+import petl
+import psycopg2
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+data_mart = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME_DM")} user={os.getenv("DB_USER_DM")} password={os.getenv("DB_PASS_DM")}')
+
+operasional = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASS")}')
+
+pekerja = petl.fromdb(operasional, "SELECT * FROM pekerja_proyek")
+jabatan = petl.fromdb(operasional, "SELECT * FROM jabatan_proyek")
+lkp_jabatan = petl.dictlookupone(jabatan, "id_jabatan")
+
+pekerja = petl.convert(pekerja, {"id_jabatan": lambda id_jabatan: lkp_jabatan[id_jabatan]["nama_jabatan"]})
+pekerja = petl.rename(pekerja, {"id_jabatan": "jabatan"})
+print(pekerja)
+# cursor = data_mart.cursor()
+# cursor.execute("TRUNCATE dim_pekerja RESTART IDENTITY CASCADE")
+# petl.todb(pekerja, cursor, "dim_pekerja")
