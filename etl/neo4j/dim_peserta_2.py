cf97b5325e75a4346d90d544eb4bc71d3570250a
HARRIS KURNIADI SUMBOGO <harris.kurniadi@ti.ukdw.ac.id>
2023-05-17 02:29:55 +0700

    etl neo4j menggunakan batch merge py2neo dan per row menggunakan execute_write

---
+++ etl/neo4j/dim_peserta_2.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from neo4j import GraphDatabase
+import petl
+import psycopg2
+import os
+
+load_dotenv()
+
+operasional = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASS")}')
+
+table_peserta = petl.fromdb(operasional, "SELECT * FROM peserta")
+table_peserta = petl.rowslice(table_peserta, 50_000)
+table_peserta = petl.dicts(table_peserta)
+
+
+def create_peserta(tx, id_peserta, nama_peserta):
+    tx.run(
+        "MERGE (a:Peserta {id_peserta: $id_peserta, nama_peserta: $nama_peserta})", id_peserta=id_peserta, nama_peserta=nama_peserta)
+
+
+uri = "neo4j://localhost:7687/"
+with GraphDatabase.driver(uri, auth=("neo4j", "@Harris99")) as driver:
+    with driver.session(database="datamart") as session:
+        for peserta in table_peserta:
+            id_peserta = peserta["id_peserta"]
+            nama_peserta = peserta["nama_peserta"]
+
+            session.execute_write(create_peserta, id_peserta, nama_peserta)
---
+++ etl/neo4j/dim_peserta_3.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from py2neo import Graph
+from py2neo.bulk import merge_nodes
+import petl
+import psycopg2
+import os
+
+load_dotenv()
+
+operasional = psycopg2.connect(
+    f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASS")}')
+
+table_peserta = petl.fromdb(operasional, "SELECT * FROM peserta")
+table_peserta = petl.rowslice(table_peserta, 50_000)
+table_peserta = petl.cutout(table_peserta, "jenis_kelamin", "tanggal_lahir")
+table_peserta = petl.dicts(table_peserta)
+
+graph = Graph("neo4j://localhost:7687/",
+              auth=("neo4j", "@Harris99"), name="datamart")
+merge_nodes(graph.auto(), table_peserta, ("Peserta", "nama_peserta"))
+print(graph.nodes.match("Peserta").count())
