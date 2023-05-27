b7b94223475806d0b79e8f7ac8c5f67a6933bbcb
HARRIS KURNIADI SUMBOGO <harris.kurniadi@ti.ukdw.ac.id>
2023-05-28 01:18:04 +0700

    menambahkan measure pada etl postgresql

--- etl/postgresql/fact_kegiatan.py
+++ etl/postgresql/fact_kegiatan.py
@@ -1,39 +1,54 @@
 import petl
 
+
 def fact_kegiatan(data_mart, operasional):
     print("===FACT KEGIATAN===")
     kegiatan = petl.fromdb(operasional, "SELECT * FROM kegiatan")
+    peserta_kegiatan = petl.fromdb(
+        operasional, "SELECT * FROM peserta_kegiatan_proyek")
+
+    lkp_peserta_kegiatan = petl.dictlookup(peserta_kegiatan, "id_kegiatan")
 
     fact_proyek = petl.fromdb(data_mart, "SELECT * FROM fact_proyek")
     dim_waktu = petl.fromdb(data_mart, "SELECT * FROM dim_waktu")
-    dim_jenis_kegiatan = petl.fromdb(data_mart, "SELECT * FROM dim_jenis_kegiatan")
-    dim_penerima_manfaat = petl.fromdb(data_mart, "SELECT * FROM dim_penerima_manfaat")
-    dim_lembaga_pelaksana = petl.fromdb(data_mart, "SELECT * FROM dim_lembaga_pelaksana")
-    dim_kabupaten_kota = petl.fromdb(data_mart, "SELECT * FROM dim_kabupaten_kota")
+    dim_jenis_kegiatan = petl.fromdb(
+        data_mart, "SELECT * FROM dim_jenis_kegiatan")
+    dim_penerima_manfaat = petl.fromdb(
+        data_mart, "SELECT * FROM dim_penerima_manfaat")
+    dim_lembaga_pelaksana = petl.fromdb(
+        data_mart, "SELECT * FROM dim_lembaga_pelaksana")
+    dim_kabupaten_kota = petl.fromdb(
+        data_mart, "SELECT * FROM dim_kabupaten_kota")
     dim_kecamatan = petl.fromdb(data_mart, "SELECT * FROM dim_kecamatan")
-    dim_desa_kelurahan = petl.fromdb(data_mart, "SELECT * FROM dim_desa_kelurahan")
+    dim_desa_kelurahan = petl.fromdb(
+        data_mart, "SELECT * FROM dim_desa_kelurahan")
 
     lkp_fact_proyek = petl.dictlookupone(fact_proyek, "id_proyek")
     lkp_dim_waktu = petl.dictlookupone(dim_waktu, "tanggal")
-    lkp_dim_jenis_kegiatan = petl.dictlookupone(dim_jenis_kegiatan, "id_jenis_kegiatan")
-    lkp_dim_penerima_manfaat = petl.dictlookupone(dim_penerima_manfaat, "id_penerima_manfaat")
-    lkp_dim_lembaga_pelaksana = petl.dictlookupone(dim_lembaga_pelaksana, "id_lembaga_pelaksana")
-    lkp_dim_kabupaten_kota = petl.dictlookupone(dim_kabupaten_kota, "id_kab_kota")
+    lkp_dim_jenis_kegiatan = petl.dictlookupone(
+        dim_jenis_kegiatan, "id_jenis_kegiatan")
+    lkp_dim_penerima_manfaat = petl.dictlookupone(
+        dim_penerima_manfaat, "id_penerima_manfaat")
+    lkp_dim_lembaga_pelaksana = petl.dictlookupone(
+        dim_lembaga_pelaksana, "id_lembaga_pelaksana")
+    lkp_dim_kabupaten_kota = petl.dictlookupone(
+        dim_kabupaten_kota, "id_kab_kota")
     lkp_dim_kecamatan = petl.dictlookupone(dim_kecamatan, "id_kecamatan")
-    lkp_dim_desa_kelurahan = petl.dictlookupone(dim_desa_kelurahan, "id_desa_kel")
+    lkp_dim_desa_kelurahan = petl.dictlookupone(
+        dim_desa_kelurahan, "id_desa_kel")
 
     fact_kegiatan = petl.convert(kegiatan,
-                                {
-                                    "id_proyek": lambda id_proyek: lkp_fact_proyek[id_proyek]["proyek_key"],
-                                    "tanggal_rencana": lambda tanggal: lkp_dim_waktu[tanggal]["waktu_key"],
-                                    "tanggal_pelaksanaan": lambda tanggal: lkp_dim_waktu[tanggal]["waktu_key"],
-                                    "id_jenis_kegiatan": lambda id_jenis_kegiatan: lkp_dim_jenis_kegiatan[id_jenis_kegiatan]["jenis_kegiatan_key"],
-                                    "id_penerima_manfaat": lambda id_penerima_manfaat: lkp_dim_penerima_manfaat[id_penerima_manfaat]["penerima_manfaat_key"],
-                                    "id_lembaga_pelaksana": lambda id_lembaga_pelaksana: lkp_dim_lembaga_pelaksana[id_lembaga_pelaksana]["lembaga_key"],
-                                    "id_kota": lambda id_kota: lkp_dim_kabupaten_kota[id_kota]["kab_kota_key"],
-                                    "id_kecamatan": lambda id_kecamatan: lkp_dim_kecamatan[id_kecamatan]["kecamatan_key"],
-                                    "id_desa": lambda id_desa: lkp_dim_desa_kelurahan[id_desa]["desa_kel_key"],
-                                })
+                                 {
+                                     "id_proyek": lambda id_proyek: lkp_fact_proyek[id_proyek]["proyek_key"],
+                                     "tanggal_rencana": lambda tanggal: lkp_dim_waktu[tanggal]["waktu_key"],
+                                     "tanggal_pelaksanaan": lambda tanggal: lkp_dim_waktu[tanggal]["waktu_key"],
+                                     "id_jenis_kegiatan": lambda id_jenis_kegiatan: lkp_dim_jenis_kegiatan[id_jenis_kegiatan]["jenis_kegiatan_key"],
+                                     "id_penerima_manfaat": lambda id_penerima_manfaat: lkp_dim_penerima_manfaat[id_penerima_manfaat]["penerima_manfaat_key"],
+                                     "id_lembaga_pelaksana": lambda id_lembaga_pelaksana: lkp_dim_lembaga_pelaksana[id_lembaga_pelaksana]["lembaga_key"],
+                                     "id_kota": lambda id_kota: lkp_dim_kabupaten_kota[id_kota]["kab_kota_key"],
+                                     "id_kecamatan": lambda id_kecamatan: lkp_dim_kecamatan[id_kecamatan]["kecamatan_key"],
+                                     "id_desa": lambda id_desa: lkp_dim_desa_kelurahan[id_desa]["desa_kel_key"],
+                                 })
 
     fact_kegiatan = petl.rename(fact_kegiatan,
                                 {
@@ -47,6 +62,12 @@ def fact_kegiatan(data_mart, operasional):
                                     "id_kecamatan": "kecamatan_key",
                                     "id_desa": "desa_kel_key",
                                 })
+
+    fact_kegiatan = petl.addfield(fact_kegiatan,
+                                  field="jumlah_peserta",
+                                  value=lambda row: len(
+                                      lkp_peserta_kegiatan[row["id_kegiatan"]])
+                                  )
 
     cursor = data_mart.cursor()
     cursor.execute("TRUNCATE fact_kegiatan RESTART IDENTITY CASCADE")
--- etl/postgresql/fact_proyek.py
+++ etl/postgresql/fact_proyek.py
@@ -1,8 +1,12 @@
 import petl
 
+
 def fact_proyek(data_mart, operasional):
     print("===FACT PROYEK===")
     proyek = petl.fromdb(operasional, "SELECT * FROM proyek")
+    kegiatan = petl.fromdb(operasional, "SELECT * FROM kegiatan")
+
+    lkp_kegiatan = petl.dictlookup(kegiatan, "id_proyek")
 
     dim_isu = petl.fromdb(data_mart, "SELECT * FROM dim_isu")
     dim_donor = petl.fromdb(data_mart, "SELECT * FROM dim_donor")
@@ -18,28 +22,36 @@ def fact_proyek(data_mart, operasional):
     lkp_dim_provinsi = petl.dictlookupone(dim_provinsi, "id_provinsi")
     lkp_dim_kota = petl.dictlookupone(dim_kota, "id_kab_kota")
 
-    fact_proyek = petl.convert(proyek, 
-                            {
-                                "id_donor": lambda id_donor: lkp_dim_donor[id_donor]["donor_key"],
-                                "id_isu": lambda id_isu: lkp_dim_isu[id_isu]["isu_key"],
-                                "id_negara": lambda id_negara: lkp_dim_negara[id_negara]["negara_key"],
-                                "id_provinsi": lambda id_provinsi: lkp_dim_provinsi[id_provinsi]["provinsi_key"],
-                                "id_kota": lambda id_kota: lkp_dim_kota[id_kota]["kab_kota_key"],
-                                "tanggal_mulai_proyek": lambda tanggal_mulai_proyek: lkp_dim_waktu[tanggal_mulai_proyek]["waktu_key"],
-                                "tanggal_selesai_proyek": lambda tanggal_selesai_proyek: lkp_dim_waktu[tanggal_selesai_proyek]["waktu_key"],
-                            })
+    fact_proyek = petl.convert(proyek,
+                               {
+                                   "id_donor": lambda id_donor: lkp_dim_donor[id_donor]["donor_key"],
+                                   "id_isu": lambda id_isu: lkp_dim_isu[id_isu]["isu_key"],
+                                   "id_negara": lambda id_negara: lkp_dim_negara[id_negara]["negara_key"],
+                                   "id_provinsi": lambda id_provinsi: lkp_dim_provinsi[id_provinsi]["provinsi_key"],
+                                   "id_kota": lambda id_kota: lkp_dim_kota[id_kota]["kab_kota_key"],
+                                   "tanggal_mulai_proyek": lambda tanggal_mulai_proyek: lkp_dim_waktu[tanggal_mulai_proyek]["waktu_key"],
+                                   "tanggal_selesai_proyek": lambda tanggal_selesai_proyek: lkp_dim_waktu[tanggal_selesai_proyek]["waktu_key"]
+                               })
 
-    fact_proyek = petl.rename(fact_proyek, 
-                            {
-                                "id_donor": "donor_key",
-                                "id_isu": "isu_key",
-                                "id_negara": "negara_key",
-                                "id_provinsi": "provinsi_key",
-                                "id_kota": "kota_key",
-                                "tanggal_mulai_proyek": "waktu_mulai_proyek_key",
-                                "tanggal_selesai_proyek": "waktu_selesai_proyek_key",
-                                "dana_anggaran": "dana_proyek"
-                            })
+    fact_proyek = petl.addfield(fact_proyek,
+                                field="jumlah_kegiatan",
+                                value=lambda row: len(lkp_kegiatan[row["id_proyek"]]))
+
+    fact_proyek = petl.addfield(fact_proyek,
+                                field="pengeluaran_proyek",
+                                value=lambda row: sum(map(lambda kegiatan: kegiatan["pengeluaran"], lkp_kegiatan[row["id_proyek"]])))
+
+    fact_proyek = petl.rename(fact_proyek,
+                              {
+                                  "id_donor": "donor_key",
+                                  "id_isu": "isu_key",
+                                  "id_negara": "negara_key",
+                                  "id_provinsi": "provinsi_key",
+                                  "id_kota": "kota_key",
+                                  "tanggal_mulai_proyek": "waktu_mulai_proyek_key",
+                                  "tanggal_selesai_proyek": "waktu_selesai_proyek_key",
+                                  "dana_anggaran": "dana_proyek"
+                              })
 
     cursor = data_mart.cursor()
     cursor.execute("TRUNCATE fact_proyek RESTART IDENTITY CASCADE")
