3722136589009b338b16540b62f054ac913b7a19
HARRIS KURNIADI <harris.kurniadi@ti.ukdw.ac.id>
2023-11-25 14:42:13 +0700

    menambahkan command keterangan fact_proyek

--- etl/postgresql/fact_proyek.py
+++ etl/postgresql/fact_proyek.py
@@ -5,13 +5,16 @@ def fact_proyek(data_mart, operasional):
     print("===FACT PROYEK===")
     proyek = petl.fromdb(operasional, "SELECT * FROM proyek")
 
+    # load seluruh dim yang dibutuhkan
     dim_isu = petl.fromdb(data_mart, "SELECT * FROM dim_isu")
     dim_donor = petl.fromdb(data_mart, "SELECT * FROM dim_donor")
     dim_waktu = petl.fromdb(data_mart, "SELECT * FROM dim_waktu")
     dim_negara = petl.fromdb(data_mart, "SELECT * FROM dim_negara")
     dim_provinsi = petl.fromdb(data_mart, "SELECT * FROM dim_provinsi")
     dim_kota = petl.fromdb(data_mart, "SELECT * FROM dim_kabupaten_kota")
-
+    
+    
+    # membuat lookup table
     lkp_dim_isu = petl.dictlookupone(dim_isu, "id_isu")
     lkp_dim_waktu = petl.dictlookupone(dim_waktu, "tanggal")
     lkp_dim_donor = petl.dictlookupone(dim_donor, "id_donor")
@@ -19,6 +22,7 @@ def fact_proyek(data_mart, operasional):
     lkp_dim_provinsi = petl.dictlookupone(dim_provinsi, "id_provinsi")
     lkp_dim_kota = petl.dictlookupone(dim_kota, "id_kab_kota")
 
+    # melakukan konversi tabel proyek menjadi fact_proyek dengan konversi id tabel yang terhubung menjadi tabel dimensi
     fact_proyek = petl.convert(proyek,
                                {
                                    "id_donor": lambda id_donor: lkp_dim_donor[id_donor]["donor_key"],
@@ -30,14 +34,20 @@ def fact_proyek(data_mart, operasional):
                                    "tanggal_selesai_proyek": lambda tanggal_selesai_proyek: lkp_dim_waktu[tanggal_selesai_proyek]["waktu_key"]
                                })
 
+    # load data fact_proyek untuk mendapatkan data last_load,
+    # last_load digunakan untuk mengetahui apakah sebuah fact_proyek pernah di load.
     fact_proyek_data = petl.fromdb(
         data_mart, "select proyek_key, id_proyek, max(tanggal_load) as last_load from fact_proyek GROUP BY proyek_key")
     lkp_fact_proyek = petl.dictlookupone(fact_proyek_data, "id_proyek")
+    
+    # mendapatkan tanggal load saat ini
     load_date = date.today()
 
+    # load data kegiatan
     kegiatan = petl.fromdb(operasional, "SELECT * FROM kegiatan")
     lkp_kegiatan = petl.dictlookup(kegiatan, "id_proyek")
 
+    # menambahkan data jumlah_kegiatan berdasarkan data fact_proyek yang terakhir di load
     fact_proyek = petl.addfield(fact_proyek,
                                 field="jumlah_kegiatan",
                                 value=lambda row: len(list(filter(lambda kegiatan:  kegiatan["tanggal_pelaksanaan"] >
@@ -45,17 +55,24 @@ def fact_proyek(data_mart, operasional):
                                                                       "last_load") or date(1, 1, 1)),
                                                                   lkp_kegiatan[row["id_proyek"]]))))
 
+    # 
     fact_proyek = petl.addfield(fact_proyek,
                                 field="pengeluaran_proyek",
-                                value=lambda row: sum(map(lambda kegiatan: kegiatan["pengeluaran"], filter(lambda kegiatan:  kegiatan["tanggal_pelaksanaan"] >
-                                                                                                           (lkp_fact_proyek.get(kegiatan["id_proyek"], {}).get(
-                                                                                                               "last_load") or date(1, 1, 1)),
-                                                                                                           lkp_kegiatan[row["id_proyek"]]))))
+                                value=lambda row: sum(
+                                    # map : mendapatkan pengeluaran dari tabel kegiatan
+                                    map(lambda kegiatan: kegiatan["pengeluaran"], 
+                                        # filter kegiatan yang tanggal_pelaksanaan nya setelah tanggal load terakhir (last_load)
+                                        filter(lambda kegiatan:  kegiatan["tanggal_pelaksanaan"] >
+                                                # mendapatkan last_load dengan lookup ke tabel fact_proyek dengan id_proyek
+                                               (lkp_fact_proyek.get(kegiatan["id_proyek"], {})
+                                                .get("last_load") or date(1, 1, 1)), lkp_kegiatan[row["id_proyek"]]))))
 
+    # menambahkan tanggal_load
     fact_proyek = petl.addfield(fact_proyek,
                                 field="tanggal_load",
                                 value=load_date)
 
+    # rename field menjadi key
     fact_proyek = petl.rename(fact_proyek,
                               {
                                   "id_donor": "donor_key",
