f44221128722d5f02f365a06fdfdee0d1366e871
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2023-12-08 22:38:12 +0700

    giving comment on load_dim postgre

--- etl/postgresql/load_dim.py
+++ etl/postgresql/load_dim.py
@@ -1,29 +1,35 @@
 import petl
 
-
+# SCD 1
 def load_dim(dm_table_name, dm_table, op_table, key_dm, key_op, cursor):
+    # lookup table for dimension
     lkp_dim = petl.dictlookupone(dm_table, key_op)
     dim_no_key = petl.cutout(dm_table, key_dm)
 
+    # get new data from operational db by lookup dimnesion
     new_data_id = petl.recordcomplement(
         petl.cut(op_table, key_op), petl.cut(dim_no_key, key_op))
     list_new_data_id = list(new_data_id[key_op])
     new_data = petl.select(
         op_table, lambda row: row[key_op] in list_new_data_id)
 
+    # add new data to dimension
     petl.appenddb(new_data, cursor, dm_table_name)
 
+    # get data from operational that have new value
     new_value = petl.recordcomplement(op_table, dim_no_key)
     new_value = petl.select(
         new_value, lambda row: row[key_op] not in list_new_data_id)
     new_value = petl.addfield(
         new_value, field=key_dm, value=lambda row: lkp_dim[row[key_op]][key_dm])
+    
+    # get column except key
     headers = petl.header(new_value)
-
     field_set = ",\n".join([
         f"{header} = %s" for header in headers[:-1]
     ])
 
+    # update all new value 
     query_update = f"""
         UPDATE {dm_table_name}
         SET
