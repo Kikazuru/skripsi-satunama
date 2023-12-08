205dd1d07d0d98d06613128760578d2b49ded3ab
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2023-12-08 23:25:31 +0700

    add create table dim penerima manfaat

--- etl/postgresql/dim_penerima_manfaat.py
+++ etl/postgresql/dim_penerima_manfaat.py
@@ -4,11 +4,22 @@ from load_dim import load_dim
 
 def dim_penerima_manfaat(data_mart, operasional):
     print("===DIM PENERIMAAN MANFAAT===")
+
+    cursor = data_mart.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS public.dim_penerima_manfaat
+    (
+        penerima_manfaat_key serial NOT NULL,
+        penerima_manfaat character varying NOT NULL,
+        id_penerima_manfaat integer,
+        CONSTRAINT dim_penerima_manfaat_pkey PRIMARY KEY (penerima_manfaat_key)
+    )
+    """)
+
     penerima_manfaat = petl.fromdb(
         operasional, "SELECT * FROM penerima_manfaat")
     dim_penerima_manfaat = petl.fromdb(
         data_mart, "SELECT * FROM dim_penerima_manfaat")
 
-    cursor = data_mart.cursor()
     load_dim("dim_penerima_manfaat", dim_penerima_manfaat, penerima_manfaat,
              "penerima_manfaat_key", "id_penerima_manfaat", cursor)
