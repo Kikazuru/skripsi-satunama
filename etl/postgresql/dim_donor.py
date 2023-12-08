5d69e266d5bd4eedddc40016782f91de556d49ce
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2023-12-08 23:50:51 +0700

    add create table dim donor

--- etl/postgresql/dim_donor.py
+++ etl/postgresql/dim_donor.py
@@ -4,6 +4,24 @@ from load_dim import load_dim
 def dim_donor(data_mart, operasional):
     print("===DIM DONOR===")
 
+    cursor = data_mart.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS public.dim_donor
+    (
+        donor_key serial NOT NULL,
+        nama_donor character varying NOT NULL,
+        negara_key integer,
+        id_donor integer,
+        singkatan character varying,
+        CONSTRAINT dim_donor_pkey PRIMARY KEY (donor_key),
+        CONSTRAINT negara FOREIGN KEY (negara_key)
+            REFERENCES public.dim_negara (negara_key) MATCH SIMPLE
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+    )
+    """)
+
     donor = petl.fromdb(operasional, "SELECT * FROM donor")
     dim_donor = petl.fromdb(data_mart, "SELECT * FROM dim_donor")
     dim_negara = petl.fromdb(data_mart, "SELECT * FROM dim_negara")
@@ -13,5 +31,4 @@ def dim_donor(data_mart, operasional):
         donor, {"id_negara": lambda id_negara: lkp_negara[id_negara]["negara_key"]})
     donor = petl.rename(donor, {"id_negara": "negara_key"})
 
-    cursor = data_mart.cursor()
     load_dim("dim_donor", dim_donor, donor, "donor_key", "id_donor", cursor)
