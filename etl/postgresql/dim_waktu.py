ea5ea95430bace35bca8ca378c401965662f3348
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2023-12-08 23:17:20 +0700

    add create table dim waktu

--- etl/postgresql/dim_waktu.py
+++ etl/postgresql/dim_waktu.py
@@ -10,6 +10,27 @@ load_dotenv()
 
 def dim_waktu(data_mart):
     print("===DIM WAKTU===")
+
+    cursor = data_mart.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS public.dim_waktu
+    (
+        waktu_key serial NOT NULL,
+        tanggal date NOT NULL,
+        hari_per_minggu integer NOT NULL,
+        hari_per_bulan integer NOT NULL,
+        bulan integer NOT NULL,
+        kuartal integer NOT NULL,
+        tahun integer NOT NULL,
+        hari_per_tahun integer NOT NULL,
+        nama_hari_per_minggu character varying NOT NULL,
+        nama_bulan character varying NOT NULL,
+        minggu_per_tahun integer NOT NULL,
+        CONSTRAINT dim_waktu_pkey PRIMARY KEY (waktu_key)
+    )
+    """)
+
     n = 50_000
     start_date = datetime.date(1900, 1, 1)
 
@@ -34,6 +55,4 @@ def dim_waktu(data_mart):
     waktu = petl.fromdicts(waktu)
     dim_waktu = petl.fromdb(data_mart, "SELECT * FROM dim_waktu")
 
-    cursor = data_mart.cursor()
-    # petl.todb(waktu, cursor, "dim_waktu")
     load_dim("dim_waktu", dim_waktu, waktu, "waktu_key", "tanggal", cursor)
