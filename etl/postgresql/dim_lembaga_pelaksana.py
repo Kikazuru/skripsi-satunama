562b60bc4a9dcb6a254f084b9ee57bda5650c50e
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2023-12-08 23:30:17 +0700

    add create table dim lembaga pelaksana

--- etl/postgresql/dim_lembaga_pelaksana.py
+++ etl/postgresql/dim_lembaga_pelaksana.py
@@ -18,6 +18,22 @@ def dim_lembaga_pelaksana(data_mart, operasional):
         lembaga_pelaksana, {"id_kota": "kota_key"})
 
     cursor = data_mart.cursor()
-    # petl.todb(lembaga_pelaksana, cursor, 'dim_lembaga_pelaksana')
+    
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS public.dim_lembaga_pelaksana
+    (
+        lembaga_key serial NOT NULL,
+        nama_lembaga character varying NOT NULL,
+        kota_key integer NOT NULL,
+        id_lembaga_pelaksana integer NOT NULL,
+        singkatan character varying,
+        CONSTRAINT dim_lembaga_pelaksana_pkey PRIMARY KEY (lembaga_key),
+        CONSTRAINT kota FOREIGN KEY (kota_key)
+            REFERENCES public.dim_kabupaten_kota (kab_kota_key) MATCH SIMPLE
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+    )
+    """)
+
     load_dim("dim_lembaga_pelaksana", dim_lembaga_pelaksana,
              lembaga_pelaksana, "lembaga_key", "id_lembaga_pelaksana", cursor)
