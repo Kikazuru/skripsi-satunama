f668108f129a46edd956349740800678bc9c770a
HARRIS KURNIADI SUMBOGO <harriskurniadi2@gmail.com>
2023-12-08 23:12:55 +0700

    add create table dim isu

--- etl/postgresql/dim_isu.py
+++ etl/postgresql/dim_isu.py
@@ -5,9 +5,19 @@ from load_dim import load_dim
 def dim_isu(data_mart, operasional):
     print("===DIM ISU===")
 
-    isu = petl.fromdb(operasional, "SELECT * FROM isu")
-    dim_isu = petl.fromdb(data_mart, "SELECT * FROM dim_isu")
-
     cursor = data_mart.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS public.dim_isu
+    (
+        isu_key serial NOT NULL,
+        nama_isu character varying NOT NULL,
+        id_isu integer,
+        CONSTRAINT dim_isu_pkey PRIMARY KEY (isu_key)
+    )
+    """)
+
+    isu = petl.fromdb(operasional, "SELECT * FROM isu")
+    dim_isu = petl.fromdb(data_mart, "SELECT * FROM dim_isu") 
 
     load_dim("dim_isu", dim_isu, isu, "isu_key", "id_isu", cursor)
